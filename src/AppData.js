f4802681d90a77fb3f2a6157678a565f476f46ea
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2023-08-12 14:34:54 +0300

    Added AppData file

---
+++ src/AppData.js
@@ -0,0 +1,34 @@
+export let hardcoded_app_data = {
+    week : {
+      "mon": [
+          { name: "dishes", done: true, assigned_to: "Joel" },
+          { name: "walk dog", done: false, assigned_to: "Marja"},
+          { name: "take out recycling", done: false, assigned_to: "Kalle"},
+      ],
+      "tue": [
+          { name: "dishes", done: true, assigned_to: "Marja" },
+          { name: "walk dog", done: false, assigned_to: "Joel"},
+      ],
+      "wed": [
+          { name: "dishes", done: true, assigned_to: "Joel" },
+          { name: "walk dog", done: false, assigned_to: "Marja"},
+      ],
+      "thu": [
+          { name: "dishes", done: true, assigned_to: "Marja" },
+          { name: "walk dog", done: false, assigned_to: "Joel"},
+      ],
+      "fri": [
+          { name: "dishes", done: true, assigned_to: "Joel" },
+          { name: "walk dog", done: false, assigned_to: "Marja"},
+          { name: "walk dog", done: false, assigned_to: "Kalle"},
+      ],
+      "sat": [
+          { name: "dishes", done: true, assigned_to: "Marja" },
+          { name: "walk dog", done: false, assigned_to: "Joel"},
+      ],
+      "sun": [
+          { name: "dishes", done: true, assigned_to: "Joel" },
+          { name: "walk dog", done: false, assigned_to: "Marja"},
+      ],
+    }
+  };
