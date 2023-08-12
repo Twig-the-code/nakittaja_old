a22105c0af631a28b47fee2369a8055afe030094
Laura Järvinen <102667765+bcjarla@users.noreply.github.com>
2023-08-12 17:27:58 +0300

    Added days to week

--- src/components/Week.js
+++ src/components/Week.js
@@ -1,8 +1,20 @@
+function Day() {
+  return (
+    <p className="day">päivä</p>
+  )
+}
 
 const Week = () => {
   return (
     <section>
       <h1>Viikkonäkymä</h1>
+      <Day />
+      <Day />
+      <Day />
+      <Day />
+      <Day />
+      <Day />
+      <Day />
     </section>
   )
 }
