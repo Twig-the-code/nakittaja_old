2ed4aabd2d3f6f5dd0cf9edfa6c851114b067a7c
kaepy <henna.tuunainen@gmail.com>
2023-08-12 16:42:25 +0300

    Fix lint warnigs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 import Tasks from './components/Tasks'
 
 function App() {
   const [tasks, setTasks] = useState([])
 
   return (
-    <div className="App">
+    <div>
       <p>Tähän tulee nakittaja</p>
       <Tasks tasks={tasks} setTasks={setTasks} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Tasks.js
@@ -0,0 +1,10 @@
+const Tasks = ({ tasks, setTasks }) => {
+
+  return (
+    <div>
+      <p>Tasks:</p>
+    </div>
+  )
+}
+
+export default Tasks
